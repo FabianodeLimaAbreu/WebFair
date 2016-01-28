6ff4e16f2a52dee6495eddd99a5daf59dea30ebc
Fabiano de Lima Abreu <fabianodelimaabreu@gmail.com>
2016-01-28 14:35:50 -0200

    Before try to fix more complicated errors

---
+++ github/js/jsdoc.js
@@ -0,0 +1,45 @@
+/**
+* Represents a book `method`.
+* @constructor
+* @param {string} title - The title of the book.
+* @param {string} author - The author of the book.
+*/
+function Book(title, author) {
+}
+
+/**
+*Função de Teste
+*@param {string} teste - Parametro de teste
+*@return {number} valor - Valor retornado
+*/
+function Teste(teste){
+
+}
+
+
+/**
+*Função de Teste1
+*@param {string} teste - Parametro de teste
+*@return {boolean}- return true
+*/
+function Teste1(teste){
+	return true;
+
+}
+
+
+/**
+*Função de Teste2
+*@param {string} teste - Parametro de teste
+*/
+function Teste2(teste){
+
+}
+
+/**
+*Função de Teste3
+*@param {string} teste - Parametro de teste
+*/
+function Teste3(teste){
+
+}
